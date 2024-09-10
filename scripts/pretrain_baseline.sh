cd1c2c61b8081cf636fcc49123bbac6d3afeed7e
v-lijunzhe <v-lijunzhe@microsoft.com>
2024-09-10 06:20:48 +0000

    FEAT: add the scripts for az ml

---
+++ scripts/pretrain_baseline.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+ulimit -c unlimited
+
+[ -z "${outdir}" ] && outdir='/sfm/sfmexpresults/junzhe/psm/edm_test/baseline'
+[ -z "${n_gpu}" ] && n_gpu=$(nvidia-smi -L | wc -l)
+echo "n_gpu: ${n_gpu}"
+
+export MKL_SERVICE_FORCE_INTEL=1
+export MKL_THREADING_LAYER='GNU'
+
+torchrun --standalone --nproc_per_node=$n_gpu edm/train.py --outdir=$outdir --data=edm/datasets/cifar10-32x32.zip --cond=1 --arch=ddpmpp
+
+sleep infinity
